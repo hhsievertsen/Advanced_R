1a090eeb76836c0351f03d27b675dd1ab06c63d3
Hans H. Sievertsen <hhsievertsen@users.noreply.github.com>
2020-06-24 14:24:35 +0100

    first draft

---
+++ advanced_R.R
@@ -0,0 +1,54 @@
+---
+title: "Intro to spreadsheet software "
+output: 
+  learnr::tutorial:
+  theme: "journal"
+css: "css/mystyle.css"
+runtime: shiny_prerendered
+---
+  
+  ```{r setup, include=FALSE}
+library(learnr)
+library(tidyverse)
+
+knitr::opts_chunk$set(echo = FALSE)
+```
+
+
+
+
+
+## Welcome!
+
+Hi! This is an intr
+
+
+
+by  Hans H. Sievertsen, University of Bristol 13/2/2020
+
+
+
+
+
+## 1. Organisation 
+
+### This is a sub topic
+
+## 2. Calculators and cells
+
+## 3. Cell referencing
+
+## 4. Using functions
+
+## 5. Understanding error messages
+
+## 6. Efficiency tips
+
+## 7. Formatting and table design
+
+## 8. Basic chart design
+
+## 9. Advanced chart design
+
+## 10. Getting out of here.
+
